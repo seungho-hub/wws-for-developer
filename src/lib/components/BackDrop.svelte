1e198ab540391061b1e19ccf42e51ee2df995b48
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-11-14 21:18:58 +0900

    add backdrop

---
+++ src/lib/components/BackDrop.svelte
@@ -0,0 +1,23 @@
+<script>
+  export let showModal;
+</script>
+
+<div
+  class="backdrop"
+  on:click={() => {
+    showModal = false;
+  }}
+>
+  <slot />
+</div>
+
+<style lang="scss">
+  .backdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.7);
+  }
+</style>
