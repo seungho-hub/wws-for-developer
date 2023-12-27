840d71b61362ee3e412e25aa9d54b2e0cc3e92cd
seungho-hub <63999413+kangseungho-hub@users.noreply.github.com>
2023-12-27 23:13:25 +0900

    feat(context-isolation)

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,20 @@ import { sveltekit } from "@sveltejs/kit/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    {
+      name: "crossOriginIsolation",
+      configureServer: (server) => {
+        server.middlewares.use((req, res, next) => {
+          res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
+          res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+
+          next();
+        });
+      },
+    },
+    sveltekit(),
+  ],
   server: {
     host: "localhost",
     port: 5173,
